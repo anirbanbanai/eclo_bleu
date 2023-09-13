c8e3b1b4f2e825a8ca74c8c2396617118d6ba6da
Anirban <anirbanchondrobanai@gmail.com>
2023-09-13 16:43:59 +0600

    home added

--- src/app/(allRoute)/page.jsx
+++ src/app/(allRoute)/page.jsx
@@ -1,24 +1,37 @@
 "use client"
+
+import SubMainpage from "@/components/SubMainPage";
+import { useEffect } from "react";
+
 // import SubMainpage from "@/components/SubMainPage";
 // import axios from "axios";
-// import { useState } from "react";
+import { useState } from "react";
 
 const page = () => {
 
     // eslint-disable-next-line react-hooks/rules-of-hooks
-    // const [data,setData] =useState()
+    const [data, setData] = useState()
 
     // axios.get("http://localhost:5000/all")
     // .then(data=>{
     //     // console.log(data.data);
     //     setData(data.data)
     // })
+
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    useEffect(() => {
+        fetch("http://localhost:5000/all")
+            .then(res => res.json())
+            .then(data => {
+                setData(data)
+            })
+    }, [])
     return (
         <div className='m-5   '>
-            gggg
-           {/* {
+           
+            {
             data?.map(m=><SubMainpage key={m._id} main={m}></SubMainpage>)
-           } */}
+           }
         </div>
     );
 };

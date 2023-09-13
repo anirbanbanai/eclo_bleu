69838483361603f96d7d9beff6b26735c25220d1
Anirban <anirbanchondrobanai@gmail.com>
2023-09-13 16:37:49 +0600

    deleted issue

--- src/app/(allRoute)/page.jsx
+++ src/app/(allRoute)/page.jsx
@@ -1,23 +1,24 @@
 "use client"
-import SubMainpage from "@/components/SubMainPage";
-import axios from "axios";
-import { useState } from "react";
+// import SubMainpage from "@/components/SubMainPage";
+// import axios from "axios";
+// import { useState } from "react";
 
 const page = () => {
 
     // eslint-disable-next-line react-hooks/rules-of-hooks
-    const [data,setData] =useState()
+    // const [data,setData] =useState()
 
-    axios.get("http://localhost:5000/all")
-    .then(data=>{
-        // console.log(data.data);
-        setData(data.data)
-    })
+    // axios.get("http://localhost:5000/all")
+    // .then(data=>{
+    //     // console.log(data.data);
+    //     setData(data.data)
+    // })
     return (
         <div className='m-5   '>
-           {
+            gggg
+           {/* {
             data?.map(m=><SubMainpage key={m._id} main={m}></SubMainpage>)
-           }
+           } */}
         </div>
     );
 };
